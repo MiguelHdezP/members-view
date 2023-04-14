d5bac6790cff142944bf71349f20b14016b3f2bc
Miguel Hernandez <miguel_hernandez1@epam.com>
2023-04-14 11:29:57 -0600

    fixing node and deploy issues

--- src/components/loaderSection/LoaderSection.jsx
+++ src/components/loaderSection/LoaderSection.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./LoaderSection.scss";
 
 export const LoaderSection = ({ customClass = "" }) => {
-  return <div className={`load-section ${customClass}`}></div>;
+  return <div className={`load-section ${customClass}`}>Loader</div>;
 };
