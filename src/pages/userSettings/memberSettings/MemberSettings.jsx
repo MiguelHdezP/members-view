c171df50bca12b6bfd9af796a92f47408af94924
Miguel Hernandez <miguel_hernandez1@epam.com>
2023-04-19 16:14:29 -0600

    -

--- src/pages/userSettings/memberSettings/MemberSettings.jsx
+++ src/pages/userSettings/memberSettings/MemberSettings.jsx
@@ -13,13 +13,15 @@ import Switch from "@mui/material/Switch";
 import Tooltip from "@mui/material/Tooltip";
 import { ClickAwayListener } from "@mui/material";
 import Modal from "@mui/material/Modal";
+import { redirect } from "../../../utils/scripts";
 
 export const MemberSettings = () => {
   const [toggleNotifActive, setToggleNotifActive] = useState(false);
   const [toggleFavs, setToggleFavs] = useState(false);
-  const label = { inputProps: { "aria-label": "Switch demo" } };
+  const label = { inputProps: { "aria-label": "leave program" } };
   const [open, setOpen] = useState(false);
-  const [openModal, setOpenModal] = React.useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [leave, setLeave] = useState(true);
 
   const handleOpen = () => setOpenModal(!openModal);
 
@@ -27,6 +29,11 @@ export const MemberSettings = () => {
 
   const handleTooltipClose = () => setOpen(false);
   const handleTooltipOpen = () => setOpen(true);
+
+  const handleLeave = () => {
+    handleOpen();
+    setLeave(false);
+  };
 
   const toggleHeaderPanels = (action) => {
     if (action === "notifi") {
@@ -45,7 +52,6 @@ export const MemberSettings = () => {
         toggleFavs={toggleFavs}
         toggleHeaderPanels={toggleHeaderPanels}
       />
-
       <div className="mobile-scroll-settings">
         <section className="settings-page">
           <div className="settings-container">
@@ -68,72 +74,96 @@ export const MemberSettings = () => {
                   </button>
                   <PrimaryButton
                     text="Yes"
-                    customClass="settings-primaryBtn-fix "
+                    customClass="settings-primaryBtn-fix"
+                    fn={() => handleLeave()}
                   />
                 </div>
               </BoxCard>
             </Modal>
-            <div className="settings-breadcrumb">
-              <Breadcrumb text="Member Settings" href="/userSettings" />
-            </div>
-            <div className="settings-enrolled">
-              <p className="text-midText text-left">Care Program</p>
-              <BoxCard customClass="settings-boxcard">
-                <p className="text-midText reset-margin text-left">
-                  Program Overview
-                </p>
-                <span className="settings-boxcard-chevron">
-                  <HiOutlineChevronRight />
-                </span>
-              </BoxCard>
-              <BoxCard customClass="settings-boxcard">
-                <p className="text-midText reset-margin text-left">Status</p>
-                <span className="settings-boxcard-chevron">
-                  <HiOutlineChevronRight />
-                </span>
-              </BoxCard>
-            </div>
-            <div className="settings-settings-controls">
-              <div className="settings-switch">
-                <Switch {...label} />
-                <span className="settings-switch-label">Pause Program</span>
-                <ClickAwayListener onClickAway={handleTooltipClose}>
-                  <Tooltip
-                    PopperProps={{
-                      disablePortal: true,
-                    }}
-                    onClose={handleTooltipClose}
-                    open={open}
-                    disableFocusListener
-                    disableHoverListener
-                    disableTouchListener
-                    title="This actions gives you a temporary break from your program reminders. "
-                  >
-                    <a
-                      onClick={handleTooltipOpen}
-                      className="settings-switch-anchor"
-                    >
-                      <IconContext.Provider
-                        value={{
-                          className: "settings-info-icon",
+            {leave ? (
+              <>
+                <div className="settings-breadcrumb">
+                  <Breadcrumb text="Member Settings" href="/userSettings" />
+                </div>
+                <div className="settings-enrolled">
+                  <p className="text-midText text-left">Care Program</p>
+                  <BoxCard customClass="settings-boxcard">
+                    <p className="text-midText reset-margin text-left">
+                      Program Overview
+                    </p>
+                    <span className="settings-boxcard-chevron">
+                      <HiOutlineChevronRight />
+                    </span>
+                  </BoxCard>
+                  <BoxCard customClass="settings-boxcard">
+                    <p className="text-midText reset-margin text-left">
+                      Status
+                    </p>
+                    <span className="settings-boxcard-chevron">
+                      <HiOutlineChevronRight />
+                    </span>
+                  </BoxCard>
+                </div>
+                <div className="settings-settings-controls">
+                  <div className="settings-switch">
+                    <Switch {...label} />
+                    <span className="settings-switch-label">Pause Program</span>
+                    <ClickAwayListener onClickAway={handleTooltipClose}>
+                      <Tooltip
+                        PopperProps={{
+                          disablePortal: true,
                         }}
+                        onClose={handleTooltipClose}
+                        open={open}
+                        disableFocusListener
+                        disableHoverListener
+                        disableTouchListener
+                        title="This actions gives you a temporary break from your program reminders. "
                       >
-                        <BsFillInfoCircleFill />
-                      </IconContext.Provider>
-                    </a>
-                  </Tooltip>
-                </ClickAwayListener>
+                        <a
+                          onClick={handleTooltipOpen}
+                          className="settings-switch-anchor"
+                        >
+                          <IconContext.Provider
+                            value={{
+                              className: "settings-info-icon",
+                            }}
+                          >
+                            <BsFillInfoCircleFill />
+                          </IconContext.Provider>
+                        </a>
+                      </Tooltip>
+                    </ClickAwayListener>
+                  </div>
+                  <button
+                    onClick={handleOpen}
+                    className="settings-btn-leave text-midText"
+                  >
+                    Request Leaving Program
+                  </button>
+                </div>
+              </>
+            ) : (
+              <div className="settings-leave-confirm">
+                <p className="text-title">We're sorry to see you go Jane!</p>
+                <p className="text-smallText">
+                  In the next few days your care manager will reach out to help
+                  facilitate your program exit progress.
+                </p>
               </div>
-              <button
-                onClick={handleOpen}
-                className="settings-btn-leave text-midText"
-              >
-                Request Leaving Program
-              </button>
-            </div>
+            )}
           </div>
         </section>
       </div>
+      {leave || (
+        <div className="settings-leave-button">
+          <PrimaryButton
+            text="Return to Home Screen"
+            fn={() => redirect("/dashboard")}
+            customClass="startS-primary-button"
+          />
+        </div>
+      )}
       <Footer />
     </MobileContainer>
   );
