b33987968ee899f76af9c435c4be06cecff4a56b
Miguel Hernandez <miguel_hernandez1@epam.com>
2023-04-19 11:51:16 -0600

    auth alignment

--- src/pages/CareManager.jsx
+++ src/pages/CareManager.jsx
@@ -17,7 +17,7 @@ export const CareManager = () => {
           }}
           height="100%"
           width="100%"
-          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FnvsoDm7b543OH6MVVKeg39%2F%25E2%25AD%2590%25EF%25B8%258FACM-Gotham-Designs%3Fnode-id%3D4307-103807%26scaling%3Dmin-zoom%26page-id%3D3773%253A66988%26starting-point-node-id%3D3905%253A125368"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FnvsoDm7b543OH6MVVKeg39%2F%25E2%25AD%2590%25EF%25B8%258FACM-Gotham-Designs%3Fnode-id%3D5201-208442%26scaling%3Dmin-zoom%26page-id%3D5201%253A207724%26starting-point-node-id%3D5201%253A208442%26show-proto-sidebar%3D1"
           allowFullScreen
           title="Care Manager"
         ></iframe>
--- src/pages/JourneyBuilder.jsx
+++ src/pages/JourneyBuilder.jsx
@@ -17,7 +17,7 @@ export const JourneyBuilder = () => {
           }}
           height="100%"
           width="100%"
-          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FnvsoDm7b543OH6MVVKeg39%2F%25E2%25AD%2590%25EF%25B8%258FACM-Gotham-Designs%3Fnode-id%3D4307-103807%26scaling%3Dmin-zoom%26page-id%3D3773%253A66988%26starting-point-node-id%3D3905%253A125368"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FnvsoDm7b543OH6MVVKeg39%2F%25E2%25AD%2590%25EF%25B8%258FACM-Gotham-Designs%3Fnode-id%3D4907-113004%26scaling%3Dmin-zoom%26page-id%3D4907%253A113003%26starting-point-node-id%3D4907%253A113004%26show-proto-sidebar%3D1"
           allowFullScreen
           title="Journey Builder Figma"
         ></iframe>
