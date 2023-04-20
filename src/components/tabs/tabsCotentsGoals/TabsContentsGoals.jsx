93d8c6b66e99327a369eb4bc9026acde2384a7da
Miguel Hernandez <miguel_hernandez1@epam.com>
2023-04-20 14:05:23 -0600

    health goals

--- src/components/tabs/tabsCotentsGoals/TabsContentsGoals.jsx
+++ src/components/tabs/tabsCotentsGoals/TabsContentsGoals.jsx
@@ -1,13 +1,68 @@
 import React from "react";
 import "./TabsContentsGoals.scss";
 import { goalsTabs } from "../../../data/mockedData";
+import { BoxCard } from "../../boxCard/BoxCard";
 
 const InProgressSection = () => {
-  return <div>Progress</div>;
+  return (
+    <div className="tabs-goals">
+      <BoxCard customClass="tabs-goals-card">
+        <p className="text-midText startS-title-text tabs-goals-textLeft">
+          Schedule virtual appointments with my care manager
+        </p>
+        <span className="text-smallText tabs-goals-pill-careplan reset-margin">
+          Care Plan
+        </span>
+      </BoxCard>
+      <BoxCard customClass="tabs-goals-card">
+        <p className="text-midText startS-title-text tabs-goals-textLeft">
+          Make healthy food choices and learn more about the diabetic diet
+        </p>
+        <span className="text-smallText tabs-goals-pill-careplan reset-margin">
+          Care Plan
+        </span>
+      </BoxCard>
+      <BoxCard customClass="tabs-goals-card startS-title-text">
+        <p className="ext-midText startS-title-text tabs-goals-textLeft">
+          Eat less fast food
+        </p>
+        <span className="text-smallText tabs-goals-pill-personal reset-margin">
+          Personal
+        </span>
+      </BoxCard>
+    </div>
+  );
 };
 
 const CompletedSection = () => {
-  return <div>Completed</div>;
+  return (
+    <div className="tabs-goals">
+      <BoxCard customClass="tabs-goals-card">
+        <p className="text-midText startS-title-text tabs-goals-textLeft">
+          Schedule virtual appointments with my care manager
+        </p>
+        <span className="text-smallText tabs-goals-pill-careplan reset-margin">
+          Care Plan
+        </span>
+      </BoxCard>
+      <BoxCard customClass="tabs-goals-card">
+        <p className="text-midText startS-title-text tabs-goals-textLeft">
+          Make healthy food choices and learn more about the diabetic diet
+        </p>
+        <span className="text-smallText tabs-goals-pill-careplan reset-margin">
+          Care Plan
+        </span>
+      </BoxCard>
+      <BoxCard customClass="tabs-goals-card startS-title-text">
+        <p className="ext-midText startS-title-text tabs-goals-textLeft">
+          Eat less fast food
+        </p>
+        <span className="text-smallText tabs-goals-pill-personal reset-margin">
+          Personal
+        </span>
+      </BoxCard>
+    </div>
+  );
 };
 
 let goalsContents = [...goalsTabs];
